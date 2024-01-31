594f5494a9875a41dc687d25a1722eb1fa38710c
marcelldac <dacmarcell@gmail.com>
2024-01-30 22:43:45 -0300

    feat: use calculator implementation

---
+++ Calculator/Program.cs
@@ -0,0 +1,8 @@
+﻿using Calculator.Services;
+
+CalculatorImplementation c = new CalculatorImplementation();
+
+int num1 = 5;
+int num2 = 15;
+
+Console.WriteLine($"{num1} + {num2} = {c.Sum(num1, num2)}");
