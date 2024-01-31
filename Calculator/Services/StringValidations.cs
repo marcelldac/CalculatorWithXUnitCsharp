3b1585cc96988a13f9e37e4617827eb2bcc93ddf
marcelldac <dacmarcell@gmail.com>
2024-01-30 22:59:56 -0300

    feat: count characters method

---
+++ Calculator/Services/StringValidations.cs
@@ -0,0 +1,11 @@
+namespace Calculator.Services
+{
+    public class StringValidations
+    {
+        public int CountCharacters(string text)
+        {
+            int num = text.Length;
+            return num;
+        }
+    }
+}
