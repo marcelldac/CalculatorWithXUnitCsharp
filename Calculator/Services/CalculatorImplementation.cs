bd8a4de2d2908b60a4454de67b64f1020fdd237b
marcelldac <dacmarcell@gmail.com>
2024-01-30 22:43:54 -0300

    feat: create calculator implementation

---
+++ Calculator/Services/CalculatorImplementation.cs
@@ -0,0 +1,10 @@
+namespace Calculator.Services
+{
+    public class CalculatorImplementation
+    {
+        public int Sum(int sum1, int sum2)
+        {
+            return sum1 + sum2;
+        }
+    }
+}
