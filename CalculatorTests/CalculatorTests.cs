e4a6e5a3e4450581b86da149f89e31be64f55ca7
marcelldac <dacmarcell@gmail.com>
2024-01-30 22:47:20 -0300

    docs: separate responsibilities with comments

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -12,9 +12,12 @@ public class CalculatorTests
     [Fact]
     public void ShouldSum5With10AndReturns15()
     {
+        //Arrange
         int num1=5;
         int num2=10;
+        //Act
         int result = _calculator.Sum(num1, num2);
+        //Assert
         Assert.Equal(15, result);
     }
 }
